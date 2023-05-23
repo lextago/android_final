268458f2a5bc9f9d4c88fa3ccd6710f5fd532644
lextago <123483213+lextago@users.noreply.github.com>
2023-05-23 13:55:40 -0500

    update main

--- main.java
+++ main.java
@@ -1,1 +1,21 @@
+// use webview to create web area
+
+WebView myWebView = (WebView) findViewById(R.id.webview);
+myWebView.loadUrl("http://www.example.com");
+
+
+// add textview, textfield and button
+// button leads to next page
+// textfield data is saved into variable, sent into firebase
+// create user class
+
+// Create an object of Firebase Database Reference
+
+DatabaseReference reference ;
+reference = FirebaseDatabase.getInstance().getReference();  
+
+// Insert the user-defined object to the database 
+
+reference.child("").setValue(user1); 
+
 
